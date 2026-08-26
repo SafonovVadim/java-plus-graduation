51bb4c81ceb593455d62dfa6b6428057bc715379
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-08-26 17:06:39 +0300

    add microservices

--- core/user-service/src/main/java/ru/practicum/controller/PublicUserController.java
+++ core/user-service/src/main/java/ru/practicum/controller/PublicUserController.java
@@ -1,6 +1,7 @@
 package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.entity.User;
@@ -14,7 +15,7 @@ public class PublicUserController implements PublicUserClient {
     private final PublicUserService userService;
 
     @Override
-    public User getUser(Long userId) {
+    public User getUser(@PathVariable Long userId) {
         return userService.findById(userId);
     }
 }
