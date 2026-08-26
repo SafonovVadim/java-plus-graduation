0b1a3bf0f6e6ccd9edecf764b75dcef13baa0b5c
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-08-26 15:32:05 +0300

    add microservices

--- core/interaction-api/src/main/java/ru/practicum/errors/CustomErrorDecoder.java
+++ core/interaction-api/src/main/java/ru/practicum/errors/CustomErrorDecoder.java
@@ -4,8 +4,18 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 
 public class CustomErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultDecoder = new Default();
+
     @Override
-    public Exception decode(String s, Response response) {
-        return null;
+    public Exception decode(String methodKey, Response response) {
+        try {
+            return defaultDecoder.decode(methodKey, response);
+        } finally {
+            try {
+                response.body().close();
+            } catch (Exception ignored) {
+            }
+        }
     }
 }
