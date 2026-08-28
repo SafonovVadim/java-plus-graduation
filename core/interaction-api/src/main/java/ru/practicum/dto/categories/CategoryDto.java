b0e4b2198598d15769107526c99fe35c8c329baa
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-08-28 15:36:43 +0300

    add microservices

--- core/interaction-api/src/main/java/ru/practicum/dto/categories/CategoryDto.java
+++ core/interaction-api/src/main/java/ru/practicum/dto/categories/CategoryDto.java
@@ -3,6 +3,7 @@ package ru.practicum.dto.categories;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +17,6 @@ public class CategoryDto {
     private Long id;
     @NotNull
     @NotBlank
-    @Max(50)
+    @Size(max = 50)
     private String name;
 }
