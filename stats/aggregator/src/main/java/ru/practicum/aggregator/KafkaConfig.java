07edf3f9d6ea4d040faa874e31af1ab63d205f28
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-09-03 17:19:45 +0300

    add recommendations

--- stats/aggregator/src/main/java/ru/practicum/aggregator/KafkaConfig.java
+++ stats/aggregator/src/main/java/ru/practicum/aggregator/KafkaConfig.java
@@ -3,13 +3,11 @@ package ru.practicum.aggregator;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -18,7 +16,6 @@ import org.springframework.kafka.core.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Configuration
@@ -34,8 +31,6 @@ public class KafkaConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-" + UUID.randomUUID());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
@@ -59,21 +54,5 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, byte[]> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
-    }
-
-    @Bean
-    public ApplicationRunner seekToEnd(ConsumerFactory<String, byte[]> consumerFactory) {
-        return args -> {
-            try (var consumer = consumerFactory.createConsumer("aggregator-reset", "reset")) {
-                var partitions = consumer.partitionsFor("stats.user-actions.v1")
-                        .stream()
-                        .map(p -> new TopicPartition(p.topic(), p.partition()))
-                        .collect(Collectors.toList());
-                consumer.assign(partitions);
-                consumer.seekToEnd(partitions);
-                consumer.commitSync();
-                log.info("Aggregator consumer offset reset to end");
-            }
-        };
     }
 }
